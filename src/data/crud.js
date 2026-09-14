3a32477a86b426b1414e6891437a2f9b83bdc39b
Murilo <thiago.ssilva2215@gmail.com>
2026-09-14 11:10:48 -0300

    template-front

---
+++ src/data/crud.js
@@ -0,0 +1,39 @@
+import { KeyRound, HardDrive } from 'lucide-react';
+
+export const examples = [
+  {
+    id: 1,
+    method: 'ApiKey',
+    verb: 'Get',
+    description: 'Lista series com api-key exposta.',
+    color: 'purple',
+    Icon: KeyRound,
+  },
+  {
+    id: 2,
+    method: 'SSR',
+    verb: 'Get',
+    description: 'Lista series renderizadas no SSR',
+    color: 'purple',
+    Icon: KeyRound,
+  },
+  {
+    id: 3,
+    method: 'Offline',
+    verb: 'Get',
+    description: 'Lista series renderizadas no modo offline',
+    color: 'purple',
+    Icon: HardDrive,
+  },
+];
+
+export const crud = [
+  {
+    id: 1,
+    method: 'ApiKey',
+    verb: 'Get',
+    description: 'Lista series com api-key exposta.',
+    color: 'purple',
+    Icon: KeyRound,
+  },
+];
